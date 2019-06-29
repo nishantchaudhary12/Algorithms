d35f2f7cb3352318de35b60653196112288a1e55
Nishant Chaudhary <chaudharynishant025@gmail.com>
2019-06-29 17:01:35 -0500

    Algorithms

---
+++ LinkedLists/DoublyLL/deleteNode.py
@@ -0,0 +1,45 @@
+#doubly linked list
+#deleting a node
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        self.prev = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+
+    def delete(self, num):
+        if not self.head:
+            print('No list exist')
+        else:
+            temp = self.head
+            if num == 1:
+                self.head = temp.next
+            else:
+                flag = True
+                count = 1
+                while count != num:
+                    temp = temp.next
+                    count += 1
+                    if not temp or count > num:
+                        print('Invalid Index')
+                        flag = False
+                        break
+                if flag:
+                    var = temp.next
+                    temp = temp.prev
+                    temp.next = var
+
+
+def main():
+    my_list = LinkedList()
+    my_list.delete(3)
+
+
+main()
